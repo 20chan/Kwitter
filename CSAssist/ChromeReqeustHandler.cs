b245fa25814fc0b2803b823c9f3c3f9573033cff
phillyai <sonagi.philly@gmail.com>
2018-09-20 20:25:21 +0900

    CefSharp 다운그레이드 후 코드 약간 옮김

---
+++ CSAssist/ChromeReqeustHandler.cs
@@ -0,0 +1,93 @@
+﻿using CefSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSAssist
+{
+    public class ChromeReqeustHandler : IRequestHandler
+    {
+        public bool CanGetCookies(IWebBrowser browserControl, IBrowser browser, IFrame frame, IRequest request)
+            => true;
+
+        public bool CanSetCookie(IWebBrowser browserControl, IBrowser browser, IFrame frame, IRequest request, Cookie cookie)
+            => true;
+
+        public bool GetAuthCredentials(IWebBrowser browserControl, IBrowser browser, IFrame frame, bool isProxy, string host, int port, string realm, string scheme, IAuthCallback callback)
+            => false;
+
+        public IResponseFilter GetResourceResponseFilter(IWebBrowser browserControl, IBrowser browser, IFrame frame, IRequest request, IResponse response)
+        {
+            
+            return null;
+        }
+
+        public bool OnBeforeBrowse(IWebBrowser browserControl, IBrowser browser, IFrame frame, IRequest request, bool isRedirect)
+            => false;
+
+        public CefReturnValue OnBeforeResourceLoad(IWebBrowser browserControl, IBrowser browser, IFrame frame, IRequest request, IRequestCallback callback)
+        {
+            if (!callback.IsDisposed)
+                callback.Dispose();
+            return CefReturnValue.Continue;
+        }
+
+        public bool OnCertificateError(IWebBrowser browserControl, IBrowser browser, CefErrorCode errorCode, string requestUrl, ISslInfo sslInfo, IRequestCallback callback)
+        {
+            if (!callback.IsDisposed)
+                callback.Dispose();
+            return false;
+        }
+
+        public bool OnOpenUrlFromTab(IWebBrowser browserControl, IBrowser browser, IFrame frame, string targetUrl, WindowOpenDisposition targetDisposition, bool userGesture)
+        {
+            return false;
+        }
+
+        public void OnPluginCrashed(IWebBrowser browserControl, IBrowser browser, string pluginPath)
+        {
+        }
+
+        public bool OnProtocolExecution(IWebBrowser browserControl, IBrowser browser, string url)
+            => false;
+
+        public bool OnQuotaRequest(IWebBrowser browserControl, IBrowser browser, string originUrl, long newSize, IRequestCallback callback)
+        {
+            if (!callback.IsDisposed)
+                callback.Dispose();
+            return false;
+        }
+
+        public void OnRenderProcessTerminated(IWebBrowser browserControl, IBrowser browser, CefTerminationStatus status)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnRenderViewReady(IWebBrowser browserControl, IBrowser browser)
+        {
+        }
+
+        public void OnResourceLoadComplete(IWebBrowser browserControl, IBrowser browser, IFrame frame, IRequest request, IResponse response, UrlRequestStatus status, long receivedContentLength)
+        {
+        }
+
+        public void OnResourceRedirect(IWebBrowser browserControl, IBrowser browser, IFrame frame, IRequest request, IResponse response, ref string newUrl)
+        {
+        }
+
+        public bool OnResourceResponse(IWebBrowser browserControl, IBrowser browser, IFrame frame, IRequest request, IResponse response)
+        {
+            return false;
+        }
+
+        public bool OnSelectClientCertificate(IWebBrowser browserControl, IBrowser browser, bool isProxy, string host, int port, X509Certificate2Collection certificates, ISelectClientCertificateCallback callback)
+        {
+            if (!callback.IsDisposed)
+                callback.Dispose();
+            return false;
+        }
+    }
+}
--- CSAssist/MainWindow.xaml.cs
+++ CSAssist/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CefSharp;
+using CefSharp.OffScreen;
 
 namespace CSAssist
 {
@@ -20,9 +22,68 @@ namespace CSAssist
     /// </summary>
     public partial class MainWindow : Window
     {
+        CefSettings cefsettings;
+        BrowserSettings browsersettings;
+        ChromiumWebBrowser browser;
         public MainWindow()
         {
             InitializeComponent();
+            InitSettings();
+            InitCef();
+        }
+
+        void InitSettings()
+        {
+            cefsettings = new CefSettings
+            {
+                CachePath = null,
+                LogSeverity = LogSeverity.Disable,
+                LogFile = null,
+                WindowlessRenderingEnabled = true,
+                CefCommandLineArgs =
+                {
+                    { "no-proxy-server"          , "1" },
+                    { "mute-audio"               , "1" },
+                    { "disable-application-cache", "1" },
+                    { "disable-extensions"       , "1" },
+                    { "disable-features"         , "AsyncWheelEvents,TouchpadAndWheelScrollLatching" },
+                    { "disable-gpu"              , "1" },
+                    { "disable-gpu-vsync"        , "1" },
+                    { "disable-gpu-compositing"  , "1" },
+                }
+            };
+            cefsettings.DisableGpuAcceleration();
+            cefsettings.SetOffScreenRenderingBestPerformanceArgs();
+
+            browsersettings = new BrowserSettings()
+            {
+                DefaultEncoding = "UTF-8",
+                WebGl = CefState.Disabled,
+                Plugins = CefState.Disabled,
+                JavascriptAccessClipboard = CefState.Disabled,
+                ImageLoading = CefState.Disabled,
+                JavascriptCloseWindows = CefState.Disabled,
+                ApplicationCache = CefState.Disabled,
+                RemoteFonts = CefState.Disabled,
+                WindowlessFrameRate = 1,
+                Databases = CefState.Disabled,
+            };
+
+            CefSharpSettings.SubprocessExitIfParentProcessClosed = true;
+            CefSharpSettings.ShutdownOnExit = true;
+            CefSharpSettings.WcfEnabled = false;
+            CefSharpSettings.Proxy = null;
+        }
+
+        void InitCef()
+        {
+            Cef.Initialize(cefsettings, false, null);
+            Cef.EnableHighDPISupport();
+
+            browser = new ChromiumWebBrowser("")
+            {
+            RequestHandler   = null 
+            };
         }
     }
 }
